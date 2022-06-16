ee358d4329582ea638ac7dc09830aab18393469b
jalilov <jalilov@jalilov-hp.home>
2022-06-16 22:47:22 +0500

    counting

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 import cv2
 from func import kernal
 cap = cv2.VideoCapture('data/obj2.mp4')
-
+y=240
+x1=560
+x2=600
+count=0
+k=0
+m=0
 while True:
     ret,frame = cap.read()
     # cv2.imwrite('frame.png', frame)
@@ -23,15 +28,34 @@ while True:
     upper = np.array([78,151,255],dtype=np.uint8)
 
     mask_roi = cv2.inRange(ROI_HSV,lower,upper)
-
+    mask_roi = cv2.morphologyEx(mask_roi, cv2.MORPH_CLOSE, kernal(10,10))
     w,h = frame.shape[:2]
     frame = cv2.resize(frame, (h//2,w//2))
     mask_roi = cv2.resize(mask_roi, (h//2,w//2))
+    # print(mask_roi.shape)
+    # break
+    n = count
+    for i in range(x1,x2):
+        if mask_roi[y][i]==255:
+            count+=1
+            m+=1
+            break
+    if n==count:
+        count=0
+        m=0
+    if count>80:
+        count=0
+        k+=1
+        print(k)
+    if m>150:
+        count=0
+    frame=cv2.putText(frame,f"COUNTING: {k}",org=(720,65),fontScale=1,fontFace=0,color=(0,0,255),thickness=2)
+    # print(mask_roi[300//2,"1150//5])
 
     cv2.imshow('frame',frame)
-    cv2.imshow('ROI',mask_roi)
+    # cv2.imshow('ROI',mask_roi)
 
-    if cv2.waitKey(10) == 27:
+    if cv2.waitKey(33) == ord('q'):
         break
 
 cv2.release()
