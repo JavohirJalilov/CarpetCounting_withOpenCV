6ef64468cfeb9af35c0f389155844c755dae4f16
Jalilov <javohirjalilov777@gmail.com>
2022-06-17 11:57:57 +0500

    bounding rectangle

--- func.py
+++ func.py
@@ -6,3 +6,11 @@ import cv2
 def kernal(x,y):
     k = np.ones((x,y),dtype=np.uint8)
     return k
+
+def max_area_idx(contours):
+  area_list = []
+  for i in range(len(contours)):
+    area_list.append(cv2.contourArea(contours[i]))
+  area_list = np.array(area_list)
+  mx_idx = area_list.argmax()
+  return mx_idx
